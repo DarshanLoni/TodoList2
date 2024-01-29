f5cd08370f0577f2384bfff51bec25a57ba60b7d
DarshanLoni <darshanloni16@gmail.com>
2024-01-29 18:15:10 +0530

    Adding task done

--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -1,11 +1,22 @@
 import React,{useState} from 'react'
 import { Todoform } from './Todoform'
-
+import {v4 as uuidv4} from 'uuid'
+import { Todo } from './Todo'
 export const TodoWrapper = () => {
     const [todos,setTodos]=useState([])
+
+    const addTodo=(todo)=>{
+        setTodos([...todos,{id: uuidv4(),task:todo,completed:false,isEditing:false}])
+        console.log(todos)
+    }
   return (
     <div className='TodoWrapper'>
-        <Todoform/>
+        <h1>Get Things done!</h1>
+        <Todoform addTodo={addTodo}/>
+        {todos.map((todo,index) =>(
+            <Todo task={todo} key={index}/>
+        ))}
     </div>
   )
 }
+ 
