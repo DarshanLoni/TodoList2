dffee576bea227a1baff7ee264e6dfdb5ceac38d
DarshanLoni <darshanloni16@gmail.com>
2024-01-31 18:42:31 +0530

    END

--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import { Todoform } from './Todoform'
 import {v4 as uuidv4} from 'uuid'
 import { Todo } from './Todo'
+import { EditTodoform } from './EditTodoForm'
 export const TodoWrapper = () => {
     const [todos,setTodos]=useState([])
 
@@ -10,12 +11,40 @@ export const TodoWrapper = () => {
      //,setTodos([...todos,{task:todo}])   FINE
         console.log(todos)
     }
+
+    const toggleComplete = id =>{
+      setTodos(todos.map(todo=>todo.id === id? {...todo,completed : !todo.completed}:todo))
+    }
+
+
+    const deleteTodo = id =>{
+      setTodos(todos.filter(todo =>todo.id !=id))
+    }
+
+    const editTodo=id=>{
+      setTodos(todos.map(todo => todo.id===id ?{ ...todo,isEditing:!todo.isEditing}:todo))
+    }
+
+    const editTask = (task , id) => {
+        setTodos(todos.map(todo => todo.id === id ? 
+          {...todo , task , isEditing : !todo.isEditing}:todos
+        ))
+    }
+
   return (
     <div className='TodoWrapper'>
-        <h1>Get Things done!</h1>
-        <Todoform addTodo={addTodo}/>
+        <h1>Get Things done!</h1> 
+        <Todoform addTodo={addTodo}/>  {/* addTodo={addTodo} is a props passing to todoform.js */}
+
         {todos.map((todo,index) =>(
-            <Todo task={todo} key={index}/>
+          todo.isEditing ? (
+          <EditTodoform editTodo={editTask} task={todo}/>            
+          ) : (
+            <Todo task={todo} key={index} 
+            toggleComplete={toggleComplete} 
+            deleteTodo={deleteTodo} 
+            editTodo={editTodo}/>   //task={todo} -- is props passing to Todo.js
+            )
         ))}
     </div>
   )
